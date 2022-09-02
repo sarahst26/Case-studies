27b16eea05646652095648726ced21a63a82e399
sarahst26 <109000514+sarahst26@users.noreply.github.com>
2022-09-02 15:40:42 +0200

    Update Digital brochures copy.sql

--- Digital brochures copy.sql
+++ Digital brochures copy.sql
@@ -1,4 +1,4 @@
--- 1. Create a VIEW -----------------------------------------------------------------------------------------
+-- 1. Create a VIEW -------------------------------------------------------------------------------------------
 create view v_publisher 
 as
 select id, cast (sector_id as int) as sector_id, partition_market
@@ -7,7 +7,20 @@ from publisher;
 select * from v_publisher;
 
 
--- Most active sector in December ----------------------------------------------------------------------------
+-- 2. Compute the CTR on sector level --------------------------------------------------------------------------
+select
+sector.name as sector_name,
+round(sum(agg_clicks_impressions.clicks)/sum(agg_clicks_impressions.impr
+essions)*100, 2) || "%" as CTR
+from agg_clicks_impressions
+join v_publisher
+on agg_clicks_impressions.publisher_id = v_publisher.id
+join sector
+on v_publisher.sector_id = sector.sector_id
+group by sector.name;
+
+
+-- 3. Most active sector in December 2021 -----------------------------------------------------------------------
 select content.pfrom, content.puntil, sector.name as sector_name,
 count(content.title) as no_titles
 from content
@@ -24,4 +37,14 @@ where
 group by sector_name
 order by no_titles desc;
 
+
+-- 4. How many brochures started per sector in May 2021 ---------------------------------------------------------
+select sector.name, count(distinct content.title)
+from content
+join v_publisher
+on content.publisher_id = v_publisher.id
+join sector
+on v_publisher.sector_id = sector.sector_id
+where content.pfrom between '2021-05-01' and '2021-05-31'
+group by sector.name;
 
